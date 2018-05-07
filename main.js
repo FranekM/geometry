fa87d83a6fab3eca96c15048962a9e8e7750fd51
Bartosz Mierzwa <bartosz.m.mierzwa@gmail.com>
2018-05-07 15:37:26 +0200

    fixes & perspective switch (hidden from ui)

--- main.js
+++ main.js
@@ -1,16 +1,27 @@
 $(document).ready(() => {
     var scene = new THREE.Scene()
 
-    var camera = new THREE.PerspectiveCamera(45, $(window).width()/$(window).height(), 0.1, 10000)
+    var camera = new THREE.PerspectiveCamera(50, $(window).width()/$(window).height(), 0.1, 10000)
     camera.position.set(0,200,200)
     camera.lookAt(scene.position)
+
+    // var cameraO = new THREE.OrthographicCamera($(window).width()/-2, $(window).width()/2, 
+    //                                         $(window).height()/2, $(window).height()/-2, 0.1, 10000)
+    // cameraO.position.set(0,200,200)
+    // cameraO.lookAt(scene.position)
 
     var renderer = new THREE.WebGLRenderer()
     renderer.setClearColor(0x000000)
     renderer.setSize($(window).width(), $(window).height())
     $('#root').append(renderer.domElement)
 
-    var controls = new THREE.OrbitControls(camera, renderer.domElement);
+    var controls = new THREE.OrbitControls(camera, renderer.domElement)
+    controls.minDistance = 10
+    controls.maxDistance = 1000
+
+    // var controlsO = new THREE.OrbitControls(cameraO, renderer.domElement)
+    // controlsO.minZoom = 1
+    // controlsO.maxZoom = 10000
 
     $(window).on('resize', () => {
         camera.aspect = $(window).width()/$(window).height()
@@ -30,6 +41,7 @@ $(document).ready(() => {
 
     })
 
+    // var perspective = $('#perspective').is(':checked')
     var height = $('#height').val()
     var gons = $('#base').val()
     var type = $('#type').val() == 'prism'
@@ -43,6 +55,10 @@ $(document).ready(() => {
         color: 0x0000ff
     })
 
+    // $('#perspective').on('change', (e) => {
+    //     perspective = $(e.target).is(':checked')
+    // })
+
     $('#height').on('input', (e) => {
         height = $('#height').val()
         create()
@@ -50,6 +66,8 @@ $(document).ready(() => {
 
     $('#base').on('input', (e) => {
         gons = $(e.target).val()
+        if (gons < 3) gons = 3
+        if (gons > 24) gons = 24
         create()
     })
 
@@ -72,9 +90,10 @@ $(document).ready(() => {
 
     function render() {
         controls.update()
+        //controlsO.update()
 
         requestAnimationFrame(render)
-        renderer.render(scene, camera)
+        renderer.render(scene, camera)//perspective ? camera : cameraO)
     }
 
     render()
